b6e0990fece46945e26ed5dea3455a3ca9627260
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-07-07 16:55:38 +0700

    update-sort-student

--- studentmanament/src/App.js
+++ studentmanament/src/App.js
@@ -1,55 +1,61 @@
-import { v4 as uuidv4 } from 'uuid'
-import StudentTable from './components/StudentTable';
-import './App.css';
+import { v4 as uuidv4 } from "uuid";
+import StudentTable from "./components/StudentTable";
+import "./App.css";
 import { useState } from "react";
 function App() {
   const studentData = [
     {
       id: uuidv4(),
-      studentName: 'Nguyễn Văn A',
-      studentClass: '12A',
+      studentName: "Nguyễn Văn A",
+      studentClass: "12A",
       math: 10,
       chem: 6,
-      phy:8, 
+      phy: 8,
     },
     {
       id: uuidv4(),
-      studentName: 'Nguyễn Văn D',
-      studentClass: '12B',
+      studentName: "Nguyễn Văn D",
+      studentClass: "12B",
       math: 6,
       chem: 9,
-      phy:2, 
+      phy: 2,
     },
     {
       id: uuidv4(),
-      studentName: 'Lê Văn B',
-      studentClass: '12G',
+      studentName: "Lê Văn B",
+      studentClass: "12G",
       math: 3,
       chem: 9,
-      phy: 2, 
+      phy: 2,
     },
     {
       id: uuidv4(),
-      studentName: 'Nguyễn Văn V',
-      studentClass: '12A',
+      studentName: "Nguyễn Văn V",
+      studentClass: "12A",
       math: 10,
       chem: 4,
-      phy: 10, 
+      phy: 10,
     },
-  ]
-  const [NewStudenEle , setNewStudent ] = useState(studentData);
-  const newStudentData = (addNewStudent) => {
-    const studentEle = {...addNewStudent, id: uuidv4(),}
-    setNewStudent([...NewStudenEle , studentEle]);
-  }
-  const deleteStudent = (id) => {
-    const filterDeleteStudent = NewStudenEle.filter((deleteStudent) => deleteStudent.id !== id )
-    setNewStudent(filterDeleteStudent);
-  }
+  ];
+  // const [NewStudenEle, setNewStudent] = useState(studentData);
+  // const newStudentData = (addNewStudent) => {
+  //   const studentEle = { ...addNewStudent, id: uuidv4() };
+  //   setNewStudent([...NewStudenEle, studentEle]);
+  // };
+  // const deleteStudent = (id) => {
+  //   const filterDeleteStudent = NewStudenEle.filter(
+  //     (deleteStudent) => deleteStudent.id !== id
+  //   );
+  //   setNewStudent(filterDeleteStudent);
+  // };
   return (
     <div className="App">
       <h1> Dự Án Quản Lí Học Sinh </h1>
-      <StudentTable studentDataEle = {NewStudenEle} newStudentData = {newStudentData} deleteStudent = {deleteStudent} />
+      <StudentTable
+        studentDataEle={studentData}
+        // newStudentData={newStudentData}
+        // deleteStudent={deleteStudent}
+      />
     </div>
   );
 }
--- studentmanament/src/components/StudentTable.js
+++ studentmanament/src/components/StudentTable.js
@@ -1,41 +1,73 @@
 import StudentRow from "./StudentRow";
+import { v4 as uuidv4 } from "uuid";
 import AddNewStudent from "./AddNewStudent";
 import { useState } from "react";
+import { FILTER_OPTIONS } from "../utils/constants";
 const StudentTable = (props) => {
-  const [sortOption, setSortOption] = useState("default");
-  const { studentDataEle = [], newStudentData, deleteStudent } = props;
-  const studentList =
-    studentDataEle &&
-    studentDataEle.map((student, index) => (
-      <StudentRow {...student} index={index} deleteStudentEle={deleteStudent} />
-    ));
-  const gpaStudent = (studentDataEle) => {
+  const [sortOption, setSortOption] = useState(FILTER_OPTIONS.DEFAULT);
+  const { studentDataEle = [] } = props;
+  const [studentTable, setStudentTable] = useState(studentDataEle);
+  const newStudentData = (addNewStudent) => {
+    const studentEle = { ...addNewStudent, id: uuidv4() };
+    setStudentTable([...studentTable, studentEle]);
+  };
+  const deleteStudent = (id) => {
+    const filterDeleteStudent = studentTable.filter(
+      (deleteStudent) => deleteStudent.id !== id
+    );
+    setStudentTable(filterDeleteStudent);
+  };
+  const gpaStudent = (studentTable) =>
+    // console.log(
     (
-      (Number(studentDataEle.math) +
-        Number(studentDataEle.chem) +
-        Number(studentDataEle.phy)) /
+      (Number(studentTable.math) +
+        Number(studentTable.chem) +
+        Number(studentTable.phy)) /
       3
     ).toFixed(1);
-  };
+  // );
   const SortStudent = (e) => {
-    // console.log(sortOption);
     setSortOption(e.target.value);
   };
-  const sortedStudent = (sortOption, studentDataEle) => {
-    const sortedStudentData = [];
-    switch (sortOption) {
-      case "Ascd":
+  const sortedStudent = (sortOption, studentTable) => {
+    let sortedStudentData = [...studentTable];
+    switch (+sortOption) {
+      case FILTER_OPTIONS.GPA_ASCENDING:
+        sortedStudentData = studentTable.sort(
+          (studentA, studentB) => gpaStudent(studentA) - gpaStudent(studentB)
+        );
         break;
-      case "Dscd":
+      case FILTER_OPTIONS.GPA_DESCENDING:
+        sortedStudentData = studentTable.sort(
+          (studentA, studentB) => gpaStudent(studentB) - gpaStudent(studentA)
+        );
         break;
-      case "aToZ":
+      case FILTER_OPTIONS.A_TO_Z:
+        sortedStudentData = studentTable.sort((studentA, studentB) =>
+          studentA.studentName
+            .toLowerCase()
+            .localeCompare(studentB.studentName.toLowerCase())
+        );
         break;
-      case "ztoA":
+      case FILTER_OPTIONS.Z_TO_A:
+        sortedStudentData = studentTable.sort((studentA, studentB) =>
+          studentB.studentName
+            .toLowerCase()
+            .localeCompare(studentA.studentName.toLowerCase())
+        );
         break;
-      case "default":
-        return studentDataEle;
+      case FILTER_OPTIONS.DEFAULT:
+      default:
+        return studentTable;
     }
+    return sortedStudentData;
   };
+  const value = sortedStudent(sortOption, studentTable);
+  const studentList =
+    studentTable &&
+    studentTable.map((student, index) => (
+      <StudentRow {...student} index={index} deleteStudentEle={deleteStudent} />
+    ));
   return (
     <div className="student-table">
       <AddNewStudent newStudent={newStudentData} />
@@ -45,13 +77,13 @@ const StudentTable = (props) => {
           onChange={SortStudent}
           value={sortOption}
         >
-          <option selected value={"default"}>
+          <option selected value={FILTER_OPTIONS.DEFAULT}>
             Sắp xếp
           </option>
-          <option value={"Ascd"}>Tăng dần</option>
-          <option value={"Dscd"}>Giảm dần</option>
-          <option value={"aToZ"}>A {"->"} Z</option>
-          <option value={"ztoA"}>Z {"->"} A</option>
+          <option value={FILTER_OPTIONS.GPA_ASCENDING}>Tăng dần</option>
+          <option value={FILTER_OPTIONS.GPA_DESCENDING}>Giảm dần</option>
+          <option value={FILTER_OPTIONS.A_TO_Z}>A {"->"} Z</option>
+          <option value={FILTER_OPTIONS.Z_TO_A}>Z {"->"} A</option>
         </select>
       </div>
       <table className="table">
