52d42572f9d2dcc9b778d2c41a502ec955d2b56c
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2023-07-16 12:21:08 +0700

    update

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -33,7 +33,7 @@ function App() {
   const GetTaskTitle = JSON.parse(localStorage.getItem("TaskTitle"));
   const [todoList, setTodoList] = useState(GetTaskTitle ?? []);
   const [EditTaskEle, setEditTaskEle] = useState("");
-
+  const [check , setCheck] = useState({})
   const onAddNewTaskHandler = (NewTask) => {
     const SaveNewTask = [...todoList, NewTask];
     const StrSaveNewTask = JSON.stringify(SaveNewTask);
@@ -49,36 +49,14 @@ function App() {
   const EditEle = (id) => {
     const filterEditTaskList = todoList.find((editTask) => editTask.id === id);
     setEditTaskEle(filterEditTaskList);
-    console.log(EditTaskEle);
+    // console.log(EditTaskEle);
   };
   const onChecked = (id) => {
-    const findCheckedTask = todoList.findIndex(
-      (checkedTask) => checkedTask.id === id
-    );
-    
-    
-    // const checkedTask = {...findCheckedTask, isCompleted: true}
-    // const updatingChecked = [...todoList, checkedTask]
-    // console.log(updatingChecked)
-      // setTodoList(checkedTask);
-
+    const findClickTask = todoList.find((task) => task.id === id )
+    console.log("findClickTask" , findClickTask);
+    setCheck(findClickTask);
+    console.log("a", check)
   };
-  // const onChecked = (id) => {
-  //   const findCheckedTask = todoList.find(
-  //     (checkedTask) => checkedTask.id === id
-  //   );
-  //   // console.log("checked", findCheckedTask)
-    
-  //   const updatingCheckedTask = todoList.map((task) => {
-  //     if(task.id === findCheckedTask.id ){
-  //       return {...findCheckedTask, isCompleted: !findCheckedTask.isCompleted}
-  //     }
-  //     else return todoList
-  //   })
-  //   console.log(updatingCheckedTask)
-  //   // setTodoList(updatingCheckedTask);
-
-  // };
   return (
     <div className="App">
       <div className="wrapper">
